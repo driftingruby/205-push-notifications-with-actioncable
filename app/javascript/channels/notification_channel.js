35e589079119ee1cffc5f61bd09449d0b7c39cb2
Dave Kimura <dave@k-innovations.net>
2020-02-13 07:35:51 -0500

    205

--- app/javascript/channels/notification_channel.js
+++ app/javascript/channels/notification_channel.js
@@ -1,16 +1,11 @@
 import consumer from "./consumer"
 
 consumer.subscriptions.create("NotificationChannel", {
-  connected() {
-    // Called when the subscription is ready for use on the server
-  },
+  connected() {},
 
-  disconnected() {
-    // Called when the subscription has been terminated by the server
-  },
+  disconnected() {},
 
   received(data) {
-    // Called when there's incoming data on the websocket for this channel
     if (Notification.permission === 'granted') {
       var title = 'Push Notification'
       var body = data
